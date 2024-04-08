8eb296bcdbb113dea3bf260e81f40e8cb91137bd
claranordquist <92157104+claranordquist@users.noreply.github.com>
2024-04-08 08:33:05 +0200

    Update 01_FastQC.sh

--- Code/01_FastQC.sh
+++ Code/01_FastQC.sh
@@ -15,10 +15,28 @@
 # fastqc -o /home/claran/genome_analysis/Analyses/01_Data_preprocessing/011_QC_trimmed_DNA -t 2 SRR4342129_1.paired.trimmed.fastq.gz
 
 ######################################
-#!/bin/bash
+# Formal stuff
+#!/bin/bash -l
+
+# SBATCH -A uppmax2024-2-7
+# SBATCH -M snowy
+# SBATCH --reservation=uppmax2024-2-7_3
+# SBATCH -p core
+# SBATCH -n 2
+# SBATCH t- 00:30:00
+# SBATCH -J fastQC_trimmed_DNA
+# SBATCH --mail-type=ALL
+# SBATCH --mail-user clara.nordquist.1217@student.uu.se
+# SBATCH --output=%x.%j.out
+
+# Defining the folders
 INPUT_FOLDER=/home/claran/genome_analysis/Data/Raw_data/DNA_trimmed
 OUTPUT_FOLDER=/home/claran/genome_analysis/Analyses/01_Data_preprocessing/011_QC_trimmed_DNA
 
+# Module loading
+module load bioinfo-tools FastQC/0.11.9
+
+# Run the analyses
 for SAMPLE in $INPUT_FOLDER/*
 do
   fastqc -o OUTPUT_FOLDER -t 2 $SAMPLE
