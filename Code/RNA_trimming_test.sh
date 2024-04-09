b17ed2ed15240a0b5829084716d4fde137b5ef2f
claranordquist <92157104+claranordquist@users.noreply.github.com>
2024-04-09 10:53:45 +0200

    Update RNA_trimming_test.sh

--- Code/RNA_trimming_test.sh
+++ Code/RNA_trimming_test.sh
@@ -1,4 +1,33 @@
+# Trimmomatic
+# Script to trim adapters and low-quality bases from the RNA reads
+
+# Input: 2 compressed fastq files, corresponding to one paired-end read
+# Output: 4 compressed fastq files, corresponding to 2 paired outputs and 2 "unpaired" outputs
+ 
+# We have paired-end reads
+# The program uses several different trimming steps. Here, we'll use:
+# ILLUMINACLIP: Cut adapters. Needs an adapter file (TruSeq3-PE.fa) depending on the type of Illumina read (here Illumina HiSeq 2000). We'll use the cutoff thresholds of 2:30:10 (mismatches, pair-end score, single-end score)
+# LEADING: Takes away bases at the start, below a quality threshold. From the paper method, they use Q<30.
+# TRAILING: Takes away bases at the end, below a quality threshold. From the paper method, they use Q<30.
+# SLIDINGWINDOW: Trims with a sliding window, cutting when the average quality falls below a given threshold. Needs a window size and min quality. In the paper, they state that they remove reads with "three or more N's with an average quality score less than Q20"
+# MINLEN: Removes reads that are shorter than a given length. In the paper, they remove <50 bp. 
+
+# Syntax 
+# java -jar <path to trimmomatic.jar> PE -threads <n> -trimlog <logfile> STEP:args
+
+######################################
+# Formal stuff
 #!/bin/bash -l
+
+#SBATCH -A uppmax2024-2-7
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH t- 00:30:00
+#SBATCH -J trimming_RNA
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user clara.nordquist.1217@student.uu.se
+#SBATCH --output=%x.%j.out
 
 # Defining the folders
 INPUT_FOLDER=/home/claran/genome_analysis/Data/Raw_data/RNA_untrimmed
@@ -6,19 +35,6 @@ OUTPUT_FOLDER=/home/claran/genome_analysis/Analyses/01_Data_preprocessing/013_RN
 
 # Module loading
 module load bioinfo-tools trimmomatic/0.39
-
-# Run the analyses
-
-# for ID in SRR4342137
-# do
-#	java -jar $TRIMMOMATIC_ROOT/trimmomatic.jar PE ${INPUT_FOLDER}/${ID}.1.fastq.gz ${INPUT_FOLDER}/${ID}.2.fastq.gz \
-#	${OUTPUT_FOLDER}/${ID}_forward_paired.fastq.gz ${OUTPUT_FOLDER}/${ID}_forward_unpaired.fastq.gz ${OUTPUT_FOLDER}/${ID}_reverse_paired.fastq.gz ${OUTPUT_FOLDER}/${ID}_reverse_unpaired.fastq.gz \
-#	ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:30 TRAILING:30 SLIDINGWINDOW:3:20 MINLEN:50
-# done
-
-# java -jar $TRIMMOMATIC_ROOT/trimmomatic.jar PE ${INPUT_FOLDER}/SRR4342137.1.fastq.gz ${INPUT_FOLDER}/SRR4342137.2.fastq.gz \
-# lane1_forward_paired.fq.gz lane1_forward_unpaired.fq.gz lane1_reverse_paired.fq.gz lane1_reverse_unpaired.fq.gz \
-# ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:30 TRAILING:30 SLIDINGWINDOW:3:20 MINLEN:50
 
 # Run the analyses
 for ID in SRR4342137 SRR4342139
