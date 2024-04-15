254b2b97973f9323c4ce583ccd452ef086e0b8e6
claranordquist <92157104+claranordquist@users.noreply.github.com>
2024-04-15 15:44:56 +0200

    Update 02_Quast.sh

--- Code/02_Quast.sh
+++ Code/02_Quast.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
 
-# SBATCH -A uppmax2024-2-7
-# SBATCH -M snowy
-# SBATCH -p core
-# SBATCH -n 2
-# SBATCH -t 01:15:00
-# SBATCH -J metagenome_evaluation
-# SBATCH --mail-type=ALL
-# SBATCH --mail-user clara.nordquist.1217@student.uu.se
-# SBATCH --output=%x.%j.out
+#SBATCH -A uppmax2024-2-7
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 01:15:00
+#SBATCH -J metagenome_evaluation
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user clara.nordquist.1217@student.uu.se
+#SBATCH --output=%x.%j.out
 
 ######################################
 
@@ -27,10 +27,9 @@
 INPUT_FOLDER=/home/claran/genome_analysis/Analyses/02_Assembly/021_Metagenome_assembly
 OUTPUT_FOLDER=/home/claran/genome_analysis/Analyses/02_Assembly/022_Assembly_evaluation
 
-# Module loading
-# module load bioinfo-tools quast/5.0.2
-# wget https://github.com/ablab/quast/releases/download/quast_5.2.0/quast-5.2.0.tar.gz
-# tar -xzf quast-5.2.0.tar.gz
-# cd quast-5.2.0
+# Get the software
+wget https://github.com/ablab/quast/releases/download/quast_5.2.0/quast-5.2.0.tar.gz
+tar -xzf quast-5.2.0.tar.gz
+cd quast-5.2.0
 
 python metaquast.py -o $OUTPUT_FOLDER --threads 2 $INPUT_FOLDER/final.contigs.fa
