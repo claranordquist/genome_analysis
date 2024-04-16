c774b6bd94efb51ab360134f4dfd1e37cb21c98e
claranordquist <92157104+claranordquist@users.noreply.github.com>
2024-04-16 14:51:02 +0200

    Update 03_CheckM.sh

--- Code/03_CheckM.sh
+++ Code/03_CheckM.sh
@@ -3,7 +3,7 @@
 #SBATCH -A uppmax2024-2-7
 #SBATCH -M snowy
 #SBATCH -p core
-#SBATCH -n 2
+#SBATCH -n 4
 #SBATCH -t 03:00:00
 #SBATCH -J binning_evaluation
 #SBATCH --mail-type=ALL
@@ -14,18 +14,23 @@
 
 # CheckM
 # To check the quality of the binning
+# I'll use the lineage_wf. Why? Because we're only interested in seeing what we can find (not in specific taxa really)
 
 # Input: fasta files for all bins (cannot contain . in the file names !)
 # Output: stats
 
-# Syntax:
+# Syntax: checkm lineage_wf [options] bin_dir output_dir
+# --reduced_tree To avoid memory overload
+# -x fa To show that the input bins are in .fa format
+# -t 4 To use all four threads allocated for the job
 
 ######################################
 
 # Defining the folders
-INPUT_FOLDER=
-OUTPUT_FOLDER=
+INPUT_FOLDER=/home/claran/genome_analysis/Analyses/03_Binning/031_Metagenome_binning
+OUTPUT_FOLDER=/home/claran/genome_analysis/Analyses/03_Binning/032_Binning_evaluation
 
 # Module loading
 module load bioinfo-tools CheckM/1.1.3
 
+checkm lineage_wf -x fa -t 4 --reduced_tree $INPUT_FOLDER $OUTPUT_FOLDER
