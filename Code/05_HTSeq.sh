0003d13897f10b421b71cd1b2e9b49a96ffa62e7
claranordquist <92157104+claranordquist@users.noreply.github.com>
2024-04-23 15:50:34 +0200

    Update 05_HTSeq.sh

--- Code/05_HTSeq.sh
+++ Code/05_HTSeq.sh
@@ -41,7 +41,9 @@ INPUT_FEATURES=/home/claran/genome_analysis/Analyses/04_Annotation
 OUTPUT_FOLDER=/home/claran/genome_analysis/Analyses/05_RNA_mapping/052_Read_counting
 
 # Module loading
-module load bioinfo-tools htseq/2.0.2
+module load bioinfo-tools htseq/2.0.2 samtools
+
+samtools index -o /home/claran/genome_analysis/Analyses/05_RNA_mapping/051_RNA_mapping/Bin_15_SRR4342137_index.bam $INPUT_ALIGNMENTS/Bin_15_SRR4342137_sorted.bam
 
 htseq-count -f bam -r pos -t CDS -o test.sam \
 $INPUT_ALIGNMENTS/Bin_15_SRR4342137_sorted.bam \
