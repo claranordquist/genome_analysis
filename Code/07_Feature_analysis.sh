739b631a5f823f55a255624493041863bbf7ac10
claranordquist <92157104+claranordquist@users.noreply.github.com>
2024-04-25 16:40:32 +0200

    Update 07_Feature_analysis.sh

--- Code/07_Feature_analysis.sh
+++ Code/07_Feature_analysis.sh
@@ -6,9 +6,6 @@
 # 1. Look into the HTSeq and select only the mappings which correspond to a feature (from the Prokka annotation)
 # 2. Count how many times each feature appears in each bin, and each environment
 # 3. Find more information about the features, at least all that the Prokka file says regarding ie function
-# 4. Summarize the results in a text file together with the proposed species name, looking something like:
-#     Bin XX, Proposed species XX, RNA XX (high/low oxygen)
-#     - Feature name: Feature count: UniProt ID: Function
 
 ######################################
 INPUT_HTSEQ=/home/claran/genome_analysis/Analyses/05_RNA_mapping/052_Read_counting
@@ -43,5 +40,32 @@ done
 # STEP 3: Look into the gene IDs and what Prokka says about them
 for BIN in 4 15 19 20
 do
-  awk 'print $2' Bin_${BIN}_SRR4342139_stats.txt
+  # Define the Prokka reference and the input stats file from above
+  PROKKA=Bin_${BIN}.tsv
+  INPUT=Bin_${BIN}_SRR4342137_stats.txt
+
+  # Create an output tsv file and add a header
+  echo 'Count\tGene_ID\tftype\tlength_bp\tgene\tEC_number\tCOG\tproduct' > Bin_${BIN}_SRR4342137.tsv
+
+  # Read the stats file, line by line
+  # Save the count and gene id, then look in the prokka file and collect the matching entries
+  # Write it all to the tsv outfile
+  while IFS= read -r LINE
+  do
+    COUNT=$(echo $LINE | awk '{print $1}')
+    GENE_ID=$(echo $LINE | awk '{print $2}')
+    grep "$GENE_ID" $PROKKA | sed -e "s/^\(.*\)/$COUNT\t \1/" >> Bin_${BIN}_SRR4342139.tsv
+  done < $INPUT
+
+  # Repeat the process for the second environment
+  INPUT=Bin_${BIN}_SRR4342139_stats.txt
+
+  echo 'Count\tGene_ID\tftype\tlength_bp\tgene\tEC_number\tCOG\tproduct' > Bin_${BIN}_SRR4342139.tsv
+
+  while IFS= read -r LINE
+  do
+    COUNT=$(echo $LINE | awk '{print $1}')
+    GENE_ID=$(echo $LINE | awk '{print $2}')
+    grep "$GENE_ID" $PROKKA | sed -e "s/^\(.*\)/$COUNT\t \1/" >> Bin_${BIN}_SRR4342139.tsv
+  done < $INPUT
 done
