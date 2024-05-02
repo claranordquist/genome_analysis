ed81cf13132343cc35b3bfd9a70b781be3f3b868
claranordquist <you@example.com>
2024-05-02 16:19:21 +0200

    Updated eggnog

--- Code/07_Eggnog.sh
+++ Code/07_Eggnog.sh
@@ -4,7 +4,7 @@
 #SBATCH -M snowy
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 01:30:00
+#SBATCH -t 08:00:00
 #SBATCH -J annotation
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user clara.nordquist.1217@student.uu.se
@@ -15,14 +15,17 @@
 # EggNOG
 # To annotate the metagenome bins, but do it with words describing the function of the features
 
-# Input: fasta file with the metagenome contigs
-# Output: 
+# Input: fasta file with the CDS as proteins, from Prokka
+# Output: polished gff-file with more info than from Prokka
 
 # Chosen bins: 15, 20, 4, 19
 
-# Syntax: emapper.py --itype CDS -i FASTA_FILE_NTS --output_dir OUTPUT_DIR --cpu 0 --decorate_gff FILE
-# The fasta file given should be the one from the Prokka annotation
-# The --decorate_gff will add to the Prokka annotation
+# Syntax: emapper.py -i faa_FILES_FROM_PROKKA --itype proteins -o output --output_dir OUTPUT_DIR --decorate_gff prokka_gff_file to be updated \
+# --data_dir path_to_data --cpu 0 --override
+# The faa file given should be the ones from the Prokka annotation
+# --itype proteins means that the faa files show the protein sequences of the CDS
+# --cpu 0 means to use all available cpus
+# --override tells it to overwrite any preexisting files with the same name
 
 ######################################
 
@@ -37,15 +40,10 @@ OUTPUT_FOLDER=/home/claran/genome_analysis/Analyses/07_Feature_analysis/071_Anno
 # Module loading
 module load bioinfo-tools eggNOG-mapper/2.1.9
 
-# Load the database
-# create_dbs.py -m diamond --dbname bacteria --taxa Bacteria
-
 # Run it for the bins
-# for BIN in 15 20 4 19
-for BIN in 15
+for BIN in 15 20 4 19
 do
   INPUT=$INPUT_FOLDER/Bin_${BIN}
   emapper.py --itype proteins -i $INPUT/Bin_${BIN}.faa --data_dir $EGGNOG_DATA_ROOT -o Bin_${BIN} --output_dir $OUTPUT_FOLDER \
   --cpu 0 --decorate_gff $INPUT/Bin_${BIN}_without_fasta.gff --override
 done
-
